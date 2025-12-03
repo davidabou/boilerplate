12841e3827abf498f02bcefa26bea8911d8f5d3d
nethris <david.abou@epitech.eu>
2025-12-03 21:52:45 +0100

    first commit

---
+++ server/utils/auth.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { betterAuth } from 'better-auth'
+import { prismaAdapter } from 'better-auth/adapters/prisma'
+
+import { PrismaClient } from '../generated/prisma/client'
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+export const prisma = new PrismaClient({ adapter })
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, { provider: 'postgresql' }),
+  baseURL: getBaseURL(),
+  emailAndPassword: { enabled: true },
+  user: {
+    additionalFields: {
+      firstname: { type: 'string' },
+      lastname: { type: 'string' }
+    },
+    deleteUser: { enabled: true }
+  }
+})
+
+function getBaseURL() {
+	let baseURL = process.env.BETTER_AUTH_URL
+
+	baseURL ??= getRequestURL(useEvent()).origin
+
+	return baseURL
+}
